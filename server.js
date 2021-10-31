633545eda7f25fefd7f252a79ce5ef580087d54f
John Phillips <jphillips37@gmail.com>
2021-10-30 21:58:43 -0400

    Switched to express backend.

---
+++ server.js
@@ -0,0 +1,19 @@
+const { urlencoded } = require("body-parser");
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("app/public"))
+
+require("./app/routes/html-routes")(app);
+
+app.listen(PORT, function(){
+    console.log("listening on port " + PORT);
+})
