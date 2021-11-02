e6fe22cd260e0d7141b79b9fc332295adf265381
John Phillips <jphillips37@gmail.com>
2021-11-01 20:30:27 -0400

    added routes for automation and web sections

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -3,5 +3,13 @@ var path = require("path");
 module.exports = function(app){
     app.get("/", function(req, res){
         res.sendfile(__dirname, "../assets/index.html");
-    })
+    });
+
+    app.get("/automation", function(req, res){
+        res.sendfile(__dirname, "");
+    });
+
+    app.get("/web", function(req, res){
+        res.sendfile(__dirname, "");
+    });
 };
