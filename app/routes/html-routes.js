da648996e287b1bfc4ffe9413c68775087c61be2
John Phillips <jphillips37@gmail.com>
2021-11-03 17:16:24 -0400

    fixed routes. App.get now works with express 4.17 syntax

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,18 +1,17 @@
 var path = require("path");
 
 module.exports = function(app){
-    app.get("/", function(req, res){
-        res.sendFile(__dirname, "index.html");
+    app.get('/', function(req, res){
+        res.sendFile(__dirname, 'index.html');
         //res.send("test");
         console.log(__dirname);
     });
 
-    app.get("/automation", function(req, res){
-        console.log(__dirname);
-        res.sendFile(__dirname, "../public/automation.html");
+    app.get('/automation', function(req, res){
+        res.sendFile('automation.html', {root: path.join(__dirname, '../public')});
     });
 
-    app.get("/web", function(req, res){
-        res.sendFile(__dirname, "");
+    app.get('/web', function(req, res){
+        res.sendFile('web.html', {root: path.join(__dirname, '../public')});
     });
 };
